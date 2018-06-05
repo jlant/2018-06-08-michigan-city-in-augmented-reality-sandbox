7b8bbba8c0e12eecfbb2c1831dac475fde88638c
Jeremiah Lant <jeremiahlant@gmail.com>
2018-06-05 13:52:45 -0400

    initial commit

---
+++ map/data/exp_hospital.js
@@ -0,0 +1,8 @@
+var exp_hospital = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "lat": 39.215871, "lon": -85.896377, "name": "Columbus Regional Health Hospital" }, "geometry": { "type": "Point", "coordinates": [ -85.8963765, 39.2158707 ] } }
+]
+}
